9f8e8257de86ce15be6b96ed056d50df9c7e7d38
Mark Spicer <spicer93@gmail.com>
2016-03-22 11:05:08 -0400

    Added a user setup script.

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configure zsh.
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i -e 's/robbyrussell/dpoggi/g' ~/.zshrc
+
+# Configure vim.
+cp vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Configure git.
+git config --global user.name "Mark Spicer"
+git config --global user.email "spicer93@gmail.com"
