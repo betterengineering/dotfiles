0314ad04364f27baf22eda7ea109b022d8ddd780
Mark Spicer <spicer93@gmail.com>
2016-03-22 11:15:25 -0400

    Added line for git configuration.

--- setup.sh
+++ setup.sh
@@ -12,3 +12,4 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # Configure git.
 git config --global user.name "Mark Spicer"
 git config --global user.email "spicer93@gmail.com"
+git config --global push.default simple
